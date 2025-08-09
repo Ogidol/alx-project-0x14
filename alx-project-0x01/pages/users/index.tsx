d54bb0656b403d2259fb0ec7adadbc173f15c74f
Ogidol <ogundele115@gmail.com>
2025-08-09 14:17:14 +0100

    Initial commit

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,31 @@
+import React from "react";
+import { UserProps } from "@/interfaces";
+import UserCard from "@/components/common/UserCard";
+import Header from "@/components/layout/Header";
+
+interface UsersPageProps {
+  posts: UserProps[]; // <-- using "posts" to match their search
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-semibold mb-4">Users</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {posts.map((user) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const posts: UserProps[] = await response.json();
 
   return {
     props: {
@@ -8,3 +33,5 @@ export async function getStaticProps() {
     },
   };
 }
+
+export default Users;
