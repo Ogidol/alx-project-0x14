4754d979891a74959c5fd7cfd858f6b5d5d21f4b
Ogidol <ogundele115@gmail.com>
2025-08-09 15:08:11 +0100

    Initial commit

---
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { UserData } from "@/interfaces";
+
+const UserModel: React.FC<UserData> = ({
+  name,
+  username,
+  email,
+  address,
+  phone,
+  website,
+  company,
+}) => {
+  return (
+    <div className="border p-4 rounded-lg shadow-md hover:shadow-lg transition duration-200">
+      <h2 className="text-xl font-bold">{name}</h2>
+      <p className="text-gray-500">@{username}</p>
+      <p className="mt-2">{email}</p>
+      <p>{phone}</p>
+      <p>
+        {address.street}, {address.city}
+      </p>
+      <p className="text-blue-500">{website}</p>
+      <div className="mt-2">
+        <p className="font-semibold">{company.name}</p>
+        <p className="text-sm italic">{company.catchPhrase}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserModel;
