8818bef9e2b88d2e72e4e65cfe7ed2ba9fa076d5
Ogidol <ogundele115@gmail.com>
2025-08-13 10:31:30 +0100

    first commit

---
+++ alx-movie-app/components/commons/Loading.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Loading: React.FC = () => {
+  return <div>Loading...</div>;
+};
+export default Loading;
---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+const MovieCard: React.Fc = () => {
+  return <div>Movie Card</div>;
+};
+export default MovieCard;
