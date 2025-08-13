1f6f9d4fcb0fff6a340a673ab66ae96712ed2526
Ogidol <ogundele115@gmail.com>
2025-08-13 10:39:19 +0100

    first commit

---
+++ alx-movie-app/components/layout/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const Button: React.FC = () => {
+  return <button>Click Me</button>;
+};
+
+export default Button;
---
+++ alx-movie-app/components/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Layout: React.FC = () => {
+  return (
+    <div>
+      <header>Header Component</header>
+      <main>Main Content</main>
+      <footer>Footer Component</footer>
+    </div>
+  );
+};
+
+export default Layout;
